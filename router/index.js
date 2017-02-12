3a166ca4293510f435cff38d42703abbb8a34398
Aym-fuhong <18829287015@163.com>
2017-02-12 20:33:44 +0800

    modified items to item

--- router/index.js
+++ router/index.js
@@ -1,9 +1,9 @@
-import items from './routers/items';
+import item from './routers/item';
 import cart from './routers/cart';
 import category from './routers/category';
 
 export default function(app) {
-    app.use('/items', items);
+    app.use('/item', item);
     app.use('/cart', cart);
     app.use('/category', category);
 }
